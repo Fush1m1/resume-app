8f315f5fcae930aea24cea87df0a182b6fb2fc60
Fush1m1 <onyx_1873@icloud.com>
2025-03-16 16:08:43 +0900

    add: cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 'use strict';
 
 const express = require('express');
-
+import cors from 'cors';  // Import the CORS module
 import { dbStatusMessage } from './repository/db';
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -10,15 +11,20 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+// Use CORS middleware
+app.use(cors({
+  origin: 'https://resume-app-client-czwndnsiq-fush1m1s-projects.vercel.app/',  // Allow only this origin
+}));
+
 // Connect to the database
-var statusMessage: string = '';
+let statusMessage: string = '';
 dbStatusMessage().then((value) => {
   statusMessage = value;
   console.log(statusMessage);
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!' + '___'+ statusMessage);
+  res.send('Hello World!' + '___' + statusMessage);
 });
 
 app.listen(PORT, HOST);
