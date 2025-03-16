76fb5f7e64106259b3a9cad1b88dd32f7f6ec8c0
Fush1m1 <onyx_1873@icloud.com>
2025-03-16 14:39:29 +0900

    add: db connection

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,23 @@
 
 const express = require('express');
 
+import { dbStatusMessage } from './repository/db';
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// Connect to the database
+var statusMessage: string = '';
+dbStatusMessage().then((value) => {
+  statusMessage = value;
+  console.log(statusMessage);
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!' + '___'+ statusMessage);
 });
 
 app.listen(PORT, HOST);
--- src/repository/db.ts
+++ src/repository/db.ts
@@ -1,29 +1,58 @@
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
-// const uri = process.env.MONGODB_URI || '';
-// const MongoClient = require('mongodb').MongoClient
+dotenv.config();
+const uri:string = process.env.MONGODB_URI || '';
+const MongoClient = require('mongodb').MongoClient
 
-// /*
-//  * Connect to the database and return a status message
-//  * @returns {Promise<string>} A status message
-//  */
-// export async function dbStatusMessage(): Promise<string> {
-//     var statusMessage = undefined;
-//     try {
-//         runDB();
-//         statusMessage = "Connected to the database!";
-//     } catch (error) {
-//         statusMessage = "Database connection failed:", error;
-//     }
-//     return statusMessage;
-// }
+/*
+ * Connect to the database and return a status message
+ * @returns {Promise<string>} A status message
+ */
+export async function dbStatusMessage(): Promise<string> {
+    var statusMessage: string = "connecting to the database...";
+    try {
+        await runDB();
+    } catch (error) {
+        statusMessage = "Database connection failed:", error;
+        return statusMessage;
+    }
+    statusMessage = "Connected to the database!";
+    return statusMessage;
+}
 
-// /*
-//  * Connect to the database
-//  */
-// async function runDB() {
-//     MongoClient.connect(uri, (err: any) => {
-//         if (err) throw err
-//     })
-// }
+/*
+ * Connect to the database
+ */
+async function runDB() {
+    //Create a MongoDB client, open a connection to DocDB; as a replica set,
+    //  and specify the read preference as secondary preferred
+
+    var client = MongoClient.connect(
+    // 'mongodb://dbConnectUser:Opt9BULgb%3CceTLfI%239pTL%23E%28O.Z%5D@sample-cluster.node.us-east-1.docdb.amazonaws.com:27017/sample-database?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
+    uri,
+    {
+    tlsCAFile: `global-bundle.pem` //Specify the DocDB; cert
+    },
+    function(err, client) {
+        if(err)
+            throw err;
+
+        //Specify the database to be used
+        var db = client.db('sample-database');
+
+        //Specify the collection to be used
+        var col = db.collection('sample-collection');
+
+        //Insert a single document
+        col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
+            //Find the document that was previously written
+            col.findOne({'hello':'Amazon DocumentDB'}, function(err, result){
+                //Print the result to the screen
+                console.log(result);
+
+                //Close the connection
+                client.close()
+            });
+        });
+    });
+}
